6f8e1ab99e14cce8bacfab49d2c36c84d7bb6116
ne0 <06franfran@gmail.com>
2025-11-06 10:00:59 +0100

    de jolies pages

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 
 import time, math, random, threading
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 
 app = Flask(__name__)
@@ -27,11 +27,11 @@ def release_slot(response):
 
 @app.route('/')
 def index():
-	return 'Bienvenue sur mon site !\n<a href="/page2">Aller sur la page n°2</a>'
+	return render_template('index.html')
 
 @app.route('/page2')
 def page2():
-	return 'Voici la page 2!! <a href="/">retour</a>'
+	return render_template('page2.html')
 
 if __name__ == '__main__':
 	app.run(debug=True, host="127.0.0.1", port=5000)
