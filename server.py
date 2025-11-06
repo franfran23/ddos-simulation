f9027325eefae8b04f17565f66abaeb7188860c8
ne0 <06franfran@gmail.com>
2025-11-06 09:06:37 +0100

    ajout d'une page 2 pour meilleure visualisation

--- server.py
+++ server.py
@@ -27,7 +27,11 @@ def release_slot(response):
 
 @app.route('/')
 def index():
-	return 'Bienvenue sur mon site !\n'
+	return 'Bienvenue sur mon site !\n<a href="/page2">Aller sur la page n°2</a>'
+
+@app.route('/page2')
+def page2():
+	return 'Voici la page 2!! <a href="/">retour</a>'
 
 if __name__ == '__main__':
 	app.run(debug=True, host="127.0.0.1", port=5000)
